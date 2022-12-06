080bc6517d173ac821f0641c5931624ac6a47628
cinthiyaantony <cinthiyaantony@gmail.com>
2022-12-06 13:29:23 -0500

    regsteps and regfeatures added

--- src/test/java/StepDefinitionsClass/RegSteps.java
+++ src/test/java/StepDefinitionsClass/RegSteps.java
@@ -1,5 +1,59 @@
 package StepDefinitionsClass;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class RegSteps {
+
+	@Given("Admin\\/User\\/Staff Logged on to LMS website")
+	public void admin_user_staff_logged_on_to_lms_website() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Admin\\/User\\/Staff sees the heading on the form as {string}")
+	public void admin_user_staff_sees_the_heading_on_the_form_as(String string) {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Admin\\/User\\/Staff sees a button with text {string} on the form")
+	public void admin_user_staff_sees_a_button_with_text_on_the_form(String string) {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Admin\\/User\\/Staff verify a text {string} on the form")
+	public void admin_user_staff_verify_a_text_on_the_form(String string) {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@When("Admin\\/User\\/Staff clicks the Log in button")
+	public void admin_user_staff_clicks_the_log_in_button() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Admin\\/User\\/Staff lands on Log in page")
+	public void admin_user_staff_lands_on_log_in_page() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@When("Admin\\/User\\/Staff enters following {string} {string} {string} {string} {string} {string} {string} {string} {string} {string} {string} {string} {string} to click signup button")
+	public void admin_user_staff_enters_following_to_click_signup_button(String string, String string2, String string3,
+			String string4, String string5, String string6, String string7, String string8, String string9,
+			String string10, String string11, String string12, String string13) {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Admin\\/User\\/Staff sees the message {string}")
+	public void admin_user_staff_sees_the_message(String string) {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
 
 }
